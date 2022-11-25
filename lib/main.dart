b9aaf55cc1bb13ecb5527ea13749b525fbdad56c
Yash <yv48183@gmail.com>
2022-11-25 11:33:18 +0530

    WorldTimeMAp

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'Pages/Home.dart';
+import 'Pages/Choose_Location.dart';
+import 'Pages/LoadingScreen.dart';
+//-----------------------------------------------------Started At 25-11-2022---------------------------------------------------------------------------------------------------------------------------
+//In this Application We have the Following 3 Pages
+/*
+*  Home  which is actually Showing the Time
+*  Loading Where we have The data Initially
+*  Screen where User Can Update the Location To find Out the Time
+*/
+void main()=>runApp(MaterialApp(
+home: Home(),//placing the
+));
+
+
+
